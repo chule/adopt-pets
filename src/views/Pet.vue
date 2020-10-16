2736fd5af9e71007c2ffb475914849db4f42733a
Igor Cuckovic <cuckovic@gmail.com>
2020-10-16 15:19:00 +0200

    vue vuex example

---
+++ src/views/Pet.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- {{ $route.params.species }}{{ $route.params.id }} -->
+    <!-- {{ cats[$route.params.id] }}
+    {{ [$route.params.species][$route.params.id].name }} -->
+    <!-- {{ this.pet($route.params.species)[$route.params.id].name }}  -->
+
+    <h1>{{ `${animal.name} (${$route.params.species})` }}</h1>
+    <p>age: {{ animal.age }}</p>
+    <p>breed: {{ animal.breed }}</p>
+    <p>weight: {{ animal.weight }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data() {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState(['cats', 'dogs'])
+  },
+  mounted() {
+    this.animal = this[this.$route.params.species][this.$route.params.id]
+  }
+}
+</script>
